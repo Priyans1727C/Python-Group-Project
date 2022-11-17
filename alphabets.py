2a0f679e25787f2ee691087461dd6df5b58d4444
ujjawal sharma <112757910+Sharmaujjawal123@users.noreply.github.com>
2022-11-17 23:45:14 +0530

    Update alphabets.py

--- alphabets.py
+++ alphabets.py
@@ -1,11 +1,12 @@
 #   A  pattern by nested loop
 
-a = int(input())
-for i in range(a+1):
+a = int(input("enter R: "))
+for i in range(a):
     for j in range(a+1):
-        if (j==0 or j==a) and (i!=0) or (i==0 or i==(a//2)+1) and (j<a and j>0):
+        if (j==0 or j==a-1) and (i!=0) or (i==0 or i==(a//2)) and (j<a-1 and j>0):
             print("*",end=" ")
         else:
             print(" ",end=" ")
     print()
+
 
